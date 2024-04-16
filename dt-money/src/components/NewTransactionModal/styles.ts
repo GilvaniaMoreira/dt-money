6aff00766f602d13401e804ac98e858e2d2a87e2
GilvaniaMoreira <moreiragil84@gmail.com>
2024-04-16 11:56:54 -0300

    feat: apply hook form

--- dt-money/src/components/NewTransactionModal/styles.ts
+++ dt-money/src/components/NewTransactionModal/styles.ts
@@ -19,21 +19,25 @@ export const Content = styled(Dialog.Content)`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
   form {
     margin-top: 2rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
+
     input {
       border-radius: 6px;
       border: 0;
       background: ${props => props.theme["gray-900"]};
       color: ${props => props.theme["gray-300"]};
       padding: 1rem;
+
       &::placeholder {
         color: ${props => props.theme["gray-500"]};
       }
     }
+
     button[type="submit"] {
       height: 50px;
       border: 0;
@@ -44,7 +48,13 @@ export const Content = styled(Dialog.Content)`
       border-radius: 6px;
       margin-top: 1.25rem;
       cursor: pointer;
-      &:hover {
+
+      &:disable {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+      
+      &:not(:disabled):hover {
         background: ${props => props.theme["green-700"]};
         transition: background-color 0.2s;
       }
@@ -74,7 +84,7 @@ interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome';
 }
 
-export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item) <TransactionTypeButtonProps>`
   background: ${props => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -85,6 +95,7 @@ export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButt
   cursor: pointer;
   border: 0;
   color: ${props => props.theme["gray-300"]};
+
   svg {
     color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
   }
@@ -93,6 +104,7 @@ export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButt
     transition: background-color 0.2s;
     background: ${props => props.theme["gray-600"]};
   }
+  
   &[data-state='checked'] {
     color: ${props => props.theme.white};
     background: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
